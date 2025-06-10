1d87d424574cd1c375f8bb05fa3f16d703726d03
wendyww9 <wendyskystar@gmail.com>
2025-06-09 20:39:36 -0700

    use studentData.map to iterate studentData and embed in StudentList

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -2,11 +2,26 @@ import './StudentList.css';
 import Student from './Student.jsx'
 
 const StudentList = () => {
-  const studentComponents = [
-    <li><Student name="Ada" email="ada@dev.org"></Student></li>,
-    <li><Student name="Soo-ah" email="sooah@dev.org"></Student></li>,
-    <li><Student name="Chrissy" email="chrissy@dev.org"></Student></li>
+  const studentData = [
+    {
+      nameData: 'Ada',
+      emailData: 'ada@dev.org'
+    },
+    {
+      nameData: 'Soo-ah',
+      emailData: 'sooah@dev.org'
+    },
+    {
+      nameData: 'Chrissy',
+      emailData: 'chrissy@dev.org'
+    }
   ];
+
+  const studentComponents = studentData.map(student => {
+    return (
+      <li><Student name={student.nameData} email={student.emailData}></Student></li>
+    );
+  });
 
   return (
     <section>
