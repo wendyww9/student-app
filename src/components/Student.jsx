3e6433c3547ea376bc0649339bd5707ddad38acd
wendyww9 <wendyskystar@gmail.com>
2025-06-11 14:18:33 -0700

    Add state and event handler to toggle with student presence

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -1,13 +1,22 @@
 import './StudentList.css';
 import PropTypes from "prop-types";
+import { useState } from 'react';
 
 const Student = (props) => {
-    return (
-        <ul>
-            <li>Nickname: {props.name}</li>
-            <li>Email: {props.email}</li>
-        </ul>
-    );
+  const [isPresent, setIsPresent] = useState(false);
+  const togglePresence = () => {
+    setIsPresent(isPresent => !isPresent);
+  };
+  const nameColor = isPresent ? 'green' : 'red';
+  return (
+    <div>
+    <ul>
+        <li className={nameColor}>Nickname: {props.name}</li>
+        <li>Email: {props.email}</li>
+    </ul>
+    <button onClick={togglePresence}>Toggle if {props.name} is present</button>
+    </div>
+  );
 };
 
 Student.propTypes = {
