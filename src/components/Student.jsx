44b76e0209e1fd0fcde1c2777bd0484b3e6cb041
wendyww9 <wendyskystar@gmail.com>
2025-06-12 00:46:54 -0700

    Lift State up

--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -3,14 +3,17 @@ import PropTypes from "prop-types";
 import { useState } from 'react';
 
 const Student = (props) => {
-  const nameColor = isPresent ? 'green' : 'red';
+  const attendanceButtonClicked = () => {
+    props.onPresenceToggle(props.id);
+  };
+  const nameColor = props.isPresent ? 'green' : 'red';
   return (
     <div>
       <ul>
         <li className={nameColor}>Nickname: {props.name}</li>
         <li>Email: {props.email}</li>
       </ul>
-      <button onClick={/*togglePresence*/}>Toggle if {props.name} is present</button>
+      <button onClick={attendanceButtonClicked}>Toggle if {props.name} is present</button>
     </div>
   );
 };
